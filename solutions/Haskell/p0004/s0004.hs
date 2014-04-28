1bacca36f085a95dea56f34b60db7d7e1e511a90
awashburn awashburn <awashburn1web@hotmail.com>
2014-04-28 14:47:50 -0500

    applied hlint suggestions, removed extraneous code, imporved list comprehension

--- solutions/Haskell/p0004/s0004.hs
+++ solutions/Haskell/p0004/s0004.hs
@@ -15,29 +15,13 @@ main = do
   then printHelp name
   else 
     let (lower,upper) = getRange $ getDigits args
-    in  print $ head . reverse . sort $ [ x*y | x <- [lower..upper], y <- [lower..upper], reverse (show(x*y)) == (show (x*y))]
---    in  print $ head $ dropWhile (not . isPalindrome) [ x*y | x <- [upper,upper-1..lower], y <- [upper,upper-1..lower] ]
---    in  print $ head $ dropWhile (not . isPalindrome) $ fMerge (*) [upper,upper-1..lower] [upper,upper-1..lower]
---    in  print $ [ x*y | x <- [upper,upper-1..lower], y <- [upper,upper-1..x] ]
---    in  print $ head $ dropWhile (not . isPalindrome) [ x*y | x <- [upper,upper-1..lower], y <- [upper,upper-1..x] ]
---    in  print $ sort $ filter (isPalindrome) [ x*y | x <- [upper,upper-1..lower], y <- [upper,upper-1..x] ]
-
-isPalindrome :: Int -> Bool
-isPalindrome n = reverse (show n) == (show n)
-
---fmerge :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
-fMerge g [] _      = []
-fMerge g _ []      = []
-fMerge g (x:xs) (y:ys) = (g x y) : (fMerge g xs ys)
+    in  print $ maximum [ x*y | x <- [lower..upper], y <- [lower..upper], isPalindrome (x*y)]
 
 getDigits :: [String] -> Int
 getDigits args =
-  if   length args  > 0
-  then read $ args !! 0 
+  if   not  $ null args
+  then read $ head args 
   else 3 --default
-
-getRange :: Int -> (Int,Int)
-getRange digits =  ( 10^(digits-1), (10^digits)-1 )
 
 printHelpParamPassed :: [String] -> Bool
 printHelpParamPassed =
@@ -48,4 +32,10 @@ printHelp name =
          ++ "  Usage: "++name++" <target>\n"
          ++ "  Calculates the largest prime factor of <target>\n"
          ++ "    <target>  :: Int (600851475143)\n")
+
+isPalindrome :: Int -> Bool
+isPalindrome n = reverse (show n) == show n
+
+getRange :: Int -> (Int,Int)
+getRange digits =  ( 10^(digits-1), (10^digits)-1 )
 
