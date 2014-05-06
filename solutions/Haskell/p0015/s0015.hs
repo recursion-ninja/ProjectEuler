96a4efb1f79308451c8114e87d25d4740ede881d
awashburn awashburn <awashburn1web@hotmail.com>
2014-05-06 09:55:55 -0500

    Added solution to problem 15

---
+++ solutions/Haskell/p0015/s0015.hs
@@ -0,0 +1,50 @@
+import Data.List
+import System.Environment
+import Text.Regex
+import Text.Regex.Base.RegexLike
+
+main :: IO ()
+main = do
+  args <- getArgs
+  name <- getProgName
+  if   printHelpParamPassed args
+  then printHelp name
+  else
+    let rows  = getRows args 
+        cols  = getCols args 
+    in  print $ totalPaths rows cols
+
+getRows :: [String] -> Int
+getRows args =
+  if   not  $ null args
+  then read $ head args 
+  else 20 --default
+
+getCols :: [String] -> Int
+getCols args =
+  if   length args  > 2
+  then read $ args !! 2 
+  else 20 --default
+
+printHelpParamPassed :: [String] -> Bool
+printHelpParamPassed =
+  any (match $ mkRegex "-+[hH](elp)?")
+
+printHelp :: String -> IO ()
+printHelp name =
+  putStrLn ("\n"
+         ++ "  Usage: "++name++" <rows> <cols> \n"
+         ++ "  Calculates the sum of number of paths which exist \n"
+         ++ "  to travel from one corner to the opposite corner  \n"
+         ++ "  of a <rows> by <cols> grid. \n"
+         ++ "    <rows>  :: Int (20)\n"
+         ++ "    <cols>  :: Int (20)\n")
+
+{-!-}
+
+totalPaths :: Int -> Int -> Integer
+totalPaths rows cols = numerator `div` denominator
+  where
+    numerator   = factorial (rows + cols)
+    denominator = factorial rows * factorial cols
+    factorial   = product . enumFromTo 1 . fromIntegral
