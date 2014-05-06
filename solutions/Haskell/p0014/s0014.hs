b9e30c9cdc4eaa88fae9219a89cc611402d5f68b
awashburn awashburn <awashburn1web@hotmail.com>
2014-05-05 19:17:56 -0500

    Added another solution

---
+++ solutions/Haskell/p0014/s0014.hs
@@ -0,0 +1,57 @@
+import System.Environment
+import Text.Regex
+import Text.Regex.Base.RegexLike
+import Data.List
+import Data.Map ((!),Map,fromAscList,toList)
+import Data.Ord
+
+{-- Memotize, but only under the limit --}
+
+main = do
+  args <- getArgs
+  name <- getProgName
+  if   printHelpParamPassed args
+  then printHelp name
+  else
+    let limit = getLimit args 
+    in  print $ collatzMaximumChain limit
+
+getLimit :: [String] -> Int
+getLimit args =
+  if   not  $ null args
+  then read $ head args 
+  else 1000000 --default
+
+printHelpParamPassed :: [String] -> Bool
+printHelpParamPassed =
+  any (match $ mkRegex "-+[hH](elp)?")
+
+printHelp name =
+  putStrLn ("\n"
+         ++ "  Usage: "++name++" <limit> \n"
+         ++ "  Calculates the number below <limit> \n"
+         ++ "  which creates the longest Colatz Chain \n"
+         ++ "    <limit>  :: Int (1000000)\n")
+
+{-!-}
+
+collatzMaximumChain :: Int -> Int
+collatzMaximumChain =
+  fst . maximumBy (comparing snd) . collatzLengths
+
+collatzLengths :: Int -> [(Int,Int)]
+collatzLengths k = toList lengthMap
+  where
+    lim = fromIntegral k
+    lengthMap = fromAscList [(i, collatzLength $ fromIntegral i) | i <- [1..k]]
+    collatzLength :: Integer -> Int
+    collatzLength x
+      | x == 1    = 0
+      | y <= lim  = 1 + (lengthMap ! fromIntegral y)
+      | otherwise = 1 +  collatzLength y
+      where
+        y = collatz x
+        collatz n
+          | even n = n `div` 2 
+          | odd  n = n * 3 + 1
+
