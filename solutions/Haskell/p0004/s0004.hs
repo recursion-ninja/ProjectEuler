c1645716385c273d3797324163b67c410761e2a6
awashburn awashburn <awashburn1web@hotmail.com>
2014-05-06 20:40:02 -0500

    Made imports more verbose, added type signitures

--- solutions/Haskell/p0004/s0004.hs
+++ solutions/Haskell/p0004/s0004.hs
@@ -1,8 +1,8 @@
-import System.Environment
-import Data.List
-import Text.Regex
-import Text.Regex.Base.RegexLike
+import System.Environment        (getArgs,getProgName)
+import Text.Regex                (mkRegex)
+import Text.Regex.Base.RegexLike (match)
 
+main :: IO ()
 main = do
   args <- getArgs
   name <- getProgName
@@ -22,12 +22,15 @@ printHelpParamPassed :: [String] -> Bool
 printHelpParamPassed =
   any (match $ mkRegex "-+[hH](elp)?")
 
+printHelp :: String -> IO ()
 printHelp name =
   putStrLn ("\n"
          ++ "  Usage: "++name++" <digits>\n"
          ++ "  Calculates the largest palindrome which is\n"
          ++ "  the product of two <digits> digit number\n"
          ++ "    <digits>  :: Int (3)\n")
+
+{-!-}
 
 isPalindrome :: Int -> Bool
 isPalindrome n = reverse (show n) == show n
