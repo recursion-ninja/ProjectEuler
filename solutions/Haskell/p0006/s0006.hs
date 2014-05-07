89bb31d10d74a613bbfa5035f38ca22f36dd517e
awashburn awashburn <awashburn1web@hotmail.com>
2014-05-06 20:35:09 -0500

    Made imports more verbose, added type signitures

--- solutions/Haskell/p0006/s0006.hs
+++ solutions/Haskell/p0006/s0006.hs
@@ -1,23 +1,26 @@
+--import Data.List
+import System.Environment        (getArgs,getProgName)
+import Text.Regex                (mkRegex)
+import Text.Regex.Base.RegexLike (match)
+
 {--
  - Notes:
  - Use closed forms:
  - Square of natural summation: (n(n+1)/2)^2
- - Square pyramidal number: n(n+1)(2n+1)/6
+ - Square pyramidal number:      n(n+1)(2n+1)/6
  - Difference can be reduced to:
  - ( n(3n-2)(n+1)^2 )/12
  -}
 
-import System.Environment
-import Data.List
-import Text.Regex
-import Text.Regex.Base.RegexLike
-
+main :: IO ()
 main = do
   args <- getArgs
   name <- getProgName
   if   printHelpParamPassed args
   then printHelp name
-  else print . reducedClosedForm $ getLimit args
+  else
+    let limit = getLimit args
+    in  print $ reducedClosedForm limit
 
 getLimit :: [String] -> Integer
 getLimit args =
@@ -25,17 +28,22 @@ getLimit args =
   then read $ head args 
   else 100 --default
 
-reducedClosedForm :: Integer -> Integer
-reducedClosedForm n = 
-  ( n*(n-1)*(n+1)*(3*n+2) ) `div` 12
-
 printHelpParamPassed :: [String] -> Bool
 printHelpParamPassed =
   any (match $ mkRegex "-+[hH](elp)?")
 
+printHelp :: String -> IO ()
 printHelp name =
   putStrLn ("\n"
          ++ "  Usage: "++name++" <limit>\n"
          ++ "  Calculates the square of the sum of natural numbers from 0 to <limit>\n"
          ++ "  minus the sum of the squares of natural numbers from 0 to <limit>\n"
          ++ "    <limit>  :: Integer (100)\n")
+
+{-!-}
+
+reducedClosedForm :: Integer -> Integer
+reducedClosedForm n = 
+  ( n*(n-1)*(n+1)*(3*n+2) ) `div` 12
+
+
