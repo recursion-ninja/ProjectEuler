61f0c4bf9739b039f2a932674451b628a40ad1a6
awashburn awashburn <awashburn1web@hotmail.com>
2015-01-07 23:07:12 -0600

    Refactored

--- solutions/Haskell/ProjectEuler/Problem_0001/Solution.hs
+++ solutions/Haskell/ProjectEuler/Problem_0001/Solution.hs
@@ -19,35 +19,38 @@ solution limit divisors
   |  limit < 1
   || null divisors' = 0
   |  otherwise = uncurry (-) $ getMinuendSubtrahend (limit-1) (reduce . elems $ fromList divisors')
-  where divisors' = filter (>0) divisors
+  where
+    divisors' = filter (>0) divisors
 
 getMinuendSubtrahend :: (Integral a) => a -> [a] -> (a,a)
-getMinuendSubtrahend l =
-   both (sum . map (naturalSumModulo l . multiple)) . inclusionExclusion
+getMinuendSubtrahend l
+  = both (sum . map (naturalSumModulo l . multiple))
+  . inclusionExclusion
 
 multiple :: Integral a => [a] -> a
 multiple = foldl1' lcm
 
 inclusionExclusion :: [b] -> ([[b]],[[b]])
-inclusionExclusion xs =
-    both (concatMap (choose xs))
-    . (
-    (,)
-    <$>  filter odd 
-    <*>  filter even
-    ) $  (enumFromTo 1 . length) xs
-    where 
-      choose :: [b] -> Int -> [[b]]
-      _      `choose` 0 = [[]]
-      []     `choose` _ =  []
-      (y:ys) `choose` k =  (y:) `fmap` (ys `choose` (k-1)) ++ ys `choose` k
+inclusionExclusion xs
+  = both (concatMap (choose xs))
+  . evenOdd $ (enumFromTo 1 . length) xs
+  where 
+    choose :: [b] -> Int -> [[b]]
+    _      `choose` 0 = [[]]
+    []     `choose` _ =  []
+    (y:ys) `choose` k =  (y:) `fmap` (ys `choose` (k-1)) ++ ys `choose` k
+
+evenOdd :: Integral a => [a] -> ([a],[a])
+evenOdd = (,) <$> filter odd <*> filter even
 
 both :: (a->b) -> (a,a) -> (b,b)
 both f = f *** f
 
 reduce :: (Integral a) => [a] -> [a]
 reduce []     = []
-reduce (x:xs) = (x:) . reduce $ filter ((/=0).(`mod` x)) xs
+reduce (x:xs) = (x:) . reduce $ filter (not . isMultiple) xs
+  where
+    isMultiple n  = n `mod` x == 0 
 
 naturalSumModulo :: (Integral a) => a -> a -> a
 naturalSumModulo l x = x*n*(n+1) `div` 2
