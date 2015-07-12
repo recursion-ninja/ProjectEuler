24ebe49a34ece03084c1e5e380fb10ffa70076a8
Recursion Ninja <github@recursion.ninja>
2015-07-11 23:44:15 -0500

    Added DefaultInput type

--- solutions/Haskell/ProjectEuler/Input.hs
+++ solutions/Haskell/ProjectEuler/Input.hs
@@ -1,31 +1,52 @@
 module ProjectEuler.Inputs
-  ( Input()
+  ( (~%~)
+  , DefaultInput()
+  , Input(..)
+  , defaultValue
+  , tooltip
   ) where
-
-{-- Fraction doesn't read correctly :( --}
 
 import Control.Applicative ((<$>))
 import Control.Arrow       ((***))
 import Data.Ratio          (Rational)
 
-data Input = Decimal  Double
-           | Fraction Rational
-           | List     [Input]
-           | Number   Integer
-  deriving (Eq)
+data DefaultInput 
+   = DefaultInput
+   { tooltip      :: String
+   , defaultValue :: Input
+   }
+   deriving (Eq)
+
+(~%~) :: String -> Input -> DefaultInput
+(~%~) = DefaultInput
+
+instance Show DefaultInput where
+  show x = unwords
+         [ tooltip x 
+         , ":: " ++ (tipe . defaultValue) x
+         , "("   ++ (show . defaultValue) x ++ ")"
+         ]
+
+data Input 
+   = D Double
+   | F Rational
+   | L [Input]
+   | I Integer
+   deriving (Eq)
 
 instance Show Input where
-  show (Decimal  x) = show x
-  show (Fraction x) = show x
-  show (List     x) = show x
-  show (Number    x) = show x
+  show (D x) = show x
+  show (F x) = show x
+  show (L x) = show x
+  show (I x) = show x
 
+{-- Fraction doesn't read correctly --}
 instance Read Input where
   readsPrec n str
-    | not $ null int  = (Number   *** id) <$> int
-    | not $ null frac = (Fraction *** id) <$> frac
-    | not $ null dec  = (Decimal  *** id) <$> dec
-    | not $ null list = (List     *** id) <$> list
+    | not $ null int  = (I *** id) <$> int
+    | not $ null frac = (F *** id) <$> frac
+    | not $ null dec  = (D *** id) <$> dec
+    | not $ null list = (L *** id) <$> list
     | otherwise       = []
     where
       int  = readsPrec n str :: [(Integer , String)]
@@ -33,3 +54,8 @@ instance Read Input where
       dec  = readsPrec n str :: [(Double  , String)]
       list = readsPrec n str :: [([Input] , String)]
 
+tipe :: Input -> String
+tipe (D  _) = "Double"
+tipe (F  _) = "Rational"
+tipe (L xs) = "[" ++ (tipe . head) xs ++ "]"
+tipe (I  _) = "Integer"
