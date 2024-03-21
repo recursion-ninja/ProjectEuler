f3b09ac5bb5839726b4b524fc14220b24228c378
Recursion Ninja <github@recursion.ninja>
2024-03-20 22:27:15 -0400

    Updating incorrect solution

--- solutions/Haskell/ProjectEuler/Problem_0684/Solution.hs
+++ solutions/Haskell/ProjectEuler/Problem_0684/Solution.hs
@@ -6,24 +6,32 @@
 {-# Language TypeFamilies #-}
 {-# Language TypeOperators #-}
 {-# Language UnboxedSums #-}
+{-# LANGUAGE DataKinds #-}
+--{-# Language KindSignatures, ScopedTypeVariables #-}
 
 module ProjectEuler.Problem_0684.Solution
   ( -- * Data types
     -- ** Inverse Digit Sum
     inverseDigitSum
+  , inverseDigitAggregation
     -- * Solver
 --  , solveBinomialCoefficientFactorizationSum
   , solution
   ) where
 
 
+import Data.Modular
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.List.NonEmpty qualified as NE
 import Data.Semigroup (Sum(..))
-import Math.NumberTheory.Primes (Prime, unPrime)
-import Math.NumberTheory.Recurrences.Linear (fibonacciPair)
-import Numeric.Natural
+--import Numeric.Natural
+import Data.Proxy
+import GHC.TypeNats hiding (Mod)
+import GHC.TypeLits.Compare
+import Data.Type.Equality ((:~:)( Refl ))
+import Data.Word
 
+type InverseDigitSumMod n = Sum (Word64 `Mod` n)
 
 {- |
 'inverseDigitSum' \( n \) computes the smallest number that has a digit sum of \( n \).
@@ -33,8 +41,13 @@ import Numeric.Natural
 >>> inverseDigitSum 10
 19
 -} 
-inverseDigitSum :: Natural -> Natural
-inverseDigitSum = undefined
+inverseDigitSum :: forall n . Modulus n => Word64 -> InverseDigitSumMod n
+inverseDigitSum val =
+    let (nines, prefix) = val `divMod` 9
+        magnitude = 10 ^ nines
+        addend1 = prefix * magnitude 
+        addend2 = magnitude - 1
+    in  Sum . toMod @n $ addend1 + addend2
 
 
 {- |
@@ -45,18 +58,46 @@ An 'inverseDigitAggregation' \( k \) is defined as:
 >>> inverseDigitAggregation 20
 1074
 -} 
-inverseDigitAggregation :: Natural -> Natural
-inverseDigitAggregation = undefined
+inverseDigitAggregation :: forall n . Modulus n => Word64 -> InverseDigitSumMod n
+inverseDigitAggregation = foldMap (inverseDigitSum @n) . enumFromTo 1
 
 
-fibonacciSequence :: NonEmpty Natural
+fibonacciSequence :: NonEmpty Word64
 fibonacciSequence =
     let gen ( a, b ) = ( b, a + b )
     in  fst <$> NE.iterate gen ( 0, 1 )
 
 
-solution :: Word -> Natural -> Natural
+{- |
+
+The Fibonacci sequence \(F_0, F_1, F_2, \ldots\) is defined recursively by \(F_{0} =0, F_{1} =1 \) and \(F_{n} =F_{n-1}+F_{n-2}\).
+>
+> Prove that
+\[
+\sum_{i=0}^{n} F_{i}=F_{n+2}-1 \qquad \text{for all } n \geq 0
+\]
+
+Use \(F_{n+1}+F_{n+2}=F_{n+3}\), to get:
+ 
+\[
+\sum_{i=0}^{n+1} F_{i}=\sum_{i=0}^{n} F_{i}+F_{n+1}=F_{n+2}-1+F_{n+1}=F_{n+1}+F_{n+2}-1=F_{n+3}-1
+\]
+
+Solution sums from \( i = 2 \) to \( \mathtt{limit} \), hence:
+
+\[
+\mathtt{solution}\left(\mathtt{limit}\right) = F_{\mathtt{limit} + 2} - 2 
+\]
+-}
+solution :: Word -> Word64 -> Word64
 solution limit modulus =
     let fibNumbers = take (fromEnum limit - 2) $ NE.drop 2 fibonacciSequence
-        aggregator = Sum . (`mod` modulus) . inverseDigitAggregation
-    in  getSum $ foldMap aggregator fibNumbers
+        aggregation :: forall n . SNat n -> Word64
+        aggregation SNat = case isLE (Proxy :: Proxy 1) (Proxy :: Proxy n) of
+          Nothing -> 0
+          Just Refl ->          
+            let aggregator :: Word64 -> InverseDigitSumMod n
+                aggregator = inverseDigitAggregation @n
+            in  unMod . getSum $ foldMap aggregator fibNumbers
+        
+    in  withSomeSNat (fromIntegral modulus) aggregation
